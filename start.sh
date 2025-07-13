277c1512a1c3965f3692c5e0fbc54fa13804a5bc
Leisure-Saas <info@theluxuryleisure.com>
2025-07-13 15:52:56 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,11 @@
-#!/bin/bash
+Bash
+
+#!/bin/sh
+
+# Menjalankan migrasi database
+echo "Running database migrations..."
+npx prisma migrate deploy
+
+# Memulai server aplikasi
+echo "Starting server..."
 node src/app.js
